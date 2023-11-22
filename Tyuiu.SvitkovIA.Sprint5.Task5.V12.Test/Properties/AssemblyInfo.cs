e6b8c0a7935f8dd10f32174914ce7b172dfa1fe6
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-11-22 22:53:56 +0500

    Task5

---
+++ Tyuiu.SvitkovIA.Sprint5.Task5.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SvitkovIA.Sprint5.Task5.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SvitkovIA.Sprint5.Task5.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7341f5c2-80c6-48f6-99b6-07c032327ca7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
