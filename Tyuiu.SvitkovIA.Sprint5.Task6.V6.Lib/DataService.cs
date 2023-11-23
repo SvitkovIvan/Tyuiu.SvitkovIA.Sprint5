18b6707ccd0f6fa1b3c9f1b6dff76d3e375c420e
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-11-23 23:18:57 +0500

    Task6

---
+++ Tyuiu.SvitkovIA.Sprint5.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SvitkovIA.Sprint5.Task6.V6.Lib
+{
+    public class DataService : ISprint5Task6V6
+    {
+        public int LoadFromDataFile(string path)
+        {
+            {
+                int count = 0;
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        foreach (char c in line)
+                        {
+                            if (c >= 'а' && c <= 'я')
+                            {
+                                count += 1;
+                            }
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
